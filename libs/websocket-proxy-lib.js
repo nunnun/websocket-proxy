b6d8bea5ddcb9997146da5fc4e754ca37ed0cf61
Hirotaka Nakajima <hiro@skyblue.me.uk>
2012-06-29 20:51:10 +0900

    ~ Use Base64 encode to reduce length of header

--- libs/websocket-proxy-lib.js
+++ libs/websocket-proxy-lib.js
@@ -1,7 +1,14 @@
 exports.loadWsChunk = function(uuid, chunk) {
-	var offset = 36;
+	var offset = 25;
+	var version = '1';
 	var buf = new Buffer(chunk.length + offset);
-	buf.write(uuid, 0, encoding = 'utf-8');
+	buf.write(version,0,'ascii');
+	var uuid_array = uuid.split('-');
+	var start = 1;
+	for(i in uuid_array){
+		buf.write(uuid_array[i],start,'base64');
+		start = start + Buffer.byteLength(uuid_array[i],'base64');
+	}
 	for ( var i = 0; i < chunk.length; i++) {
 		buf[offset + i] = chunk[i];
 	}
@@ -9,9 +16,17 @@ exports.loadWsChunk = function(uuid, chunk) {
 };
 
 exports.unloadWsChunk = function(chunk) {
-	var offset = 36;
-	var id = chunk.toString('utf-8', start = 0, end = offset);
+	var offset = 25;
+	var version = chunk.toString('ascii',start = 0,end = 1);
+	var uuid_array = [];
+	uuid_array.push(chunk.toString('base64',start=1,end=7));
+	uuid_array.push(chunk.toString('base64',start=7,end=10));
+	uuid_array.push(chunk.toString('base64',start=10,end=13));
+	uuid_array.push(chunk.toString('base64',start=13,end=16));
+	uuid_array.push(chunk.toString('base64',start=16,end=25));
+	var id = uuid_array.join('-');
 	return {
+		version:version,
 		id : id,
 		payload : chunk.slice(offset)
 	};
