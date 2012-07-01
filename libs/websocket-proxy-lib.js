ca980283a9d6ba95ba785d0f47a72308b87d1ff8
Hirotaka Nakajima <hiro@skyblue.me.uk>
2012-07-02 01:15:42 +0900

    ~ fix bit calculate bugs

--- libs/websocket-proxy-lib.js
+++ libs/websocket-proxy-lib.js
@@ -6,21 +6,10 @@ exports.loadWsChunk = function(id, chunk,opcode,seq) {
 	var buf = new Buffer(chunk.length + offset);
 	buf[0] = parseInt(header.toString(10));
 	
-	var hexSeq = parseInt(id, '10').toString(16);
-	var seq1,seq2;
-	if(hexSeq.length > 2){
-		seq1 = '';
-		for(var i=0;i<hexSeq.length-2;i++){
-			seq1 += hexSeq[i];
-		}
-		seq2 = hexSeq[hexSeq.length-2]+ hexSeq[hexSeq.length-1];
-	}else{
-		seq1 = '00';
-		seq2 = hexSeq;
-	}
-	buf[1] = parseInt(parseInt(seq1,'16').toString(10));
-	buf[2] = parseInt(parseInt(seq2,'16').toString(10));
-	buf[3] = seq;	
+	var id = parseInt(id, '10');
+	buf[1] = id >> 8;
+	buf[2] = id & 0xff;
+	buf[3] = seq;
 	
 	for ( var i = 0; i < chunk.length; i++) {
 		buf[offset + i] = chunk[i];
@@ -33,17 +22,8 @@ exports.unloadWsChunk = function(chunk) {
 	var header = '' + chunk[0];
 	var version = parseInt(header[0], 10).toString(16);
 	var opcode = parseInt(header[1], 10).toString(16);
-	var hexSeq ='';
-	if(chunk[1] > 0){
-		hexSeq += parseInt(chunk[1],10).toString();
-	}
-	if(parseInt(chunk[2],10).toString(16).length < 2){
-		hexSeq += '0' + parseInt(chunk[2],10).toString(16);
-	}else{
-		hexSeq += parseInt(chunk[2],10).toString(16);
-	}
-	var id = parseInt(parseInt(hexSeq,16).toString(10));
-
+	var id = (parseInt(chunk[1],10) << 8) + parseInt(chunk[2],10);
+	
 	return {
 		version:version,
 		opcode:opcode,
